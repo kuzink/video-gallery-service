0a131d946f61a392d277a59cf0a1a8b6080b5b09
kuzink <kuzink11@gmail.com>
2020-11-03 00:32:18 +0300

    TA-07 Mod: changed items to cards with images

--- browser-app/src/components/item/ItemsComponent.jsx
+++ browser-app/src/components/item/ItemsComponent.jsx
@@ -1,26 +1,31 @@
 import React from "react";
+import DefaultImage from "../../assets/defaultImage.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
 
 const ItemsComponent = (props) => {
 
 	const {items, handleOnItemSelect} = props;
 
 	return (
-		<div className="container">
-			<div className="row">
-				<div className="col-md-12 bg-white rounded shadow-sm mt-4">
-
-					<div className="py-4">
-						<h4>Items</h4>
-						{items.map(item => (
-							<div>
-								<hr/>
-								<p className="d-inline-block">Item (id={item.id}, name={item.name})</p>
-								<button className="float-right btn btn-primary"
-								        onClick={handleOnItemSelect.bind(this, item.id)}>Play</button>
+		<div className="album py-5 bg-light">
+			<div className="container">
+				<div className="row">
+					{items.map(item => (
+					<div className="col-md-4">
+						<div className="card mb-4 shadow-sm">
+							<div className="custom-card-image">
+								<img src="https://mdbootstrap.com/img/Photos/Others/forest-sm.jpg" className="card-img-top"/>
+								<div className="custom-mask flex-center" onClick={handleOnItemSelect.bind(this, item.id)}>
+									<h1><FontAwesomeIcon icon={faPlay} className="text-white "/></h1>
+								</div>
 							</div>
-						))}
+							<div className="card-body px-3 py-2">
+								<p className="card-text">{item.name}</p>
+							</div>
+						</div>
 					</div>
-
+					))}
 				</div>
 			</div>
 		</div>
