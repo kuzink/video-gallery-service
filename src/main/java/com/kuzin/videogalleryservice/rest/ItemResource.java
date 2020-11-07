ca8371a9ba432081a1ece792195ed21221d4b1b5
kuzink <kuzink11@gmail.com>
2020-11-07 19:41:58 +0300

    TA-13 Rem: ItemService, image and video links from Constants

--- src/main/java/com/kuzin/videogalleryservice/rest/ItemResource.java
+++ src/main/java/com/kuzin/videogalleryservice/rest/ItemResource.java
@@ -1,34 +1,23 @@
 package com.kuzin.videogalleryservice.rest;
 
 import com.kuzin.videogalleryservice.domain.Item;
-import com.kuzin.videogalleryservice.service.ItemService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/items")
 public class ItemResource {
 
-	private final ItemService itemService;
-
+	private final List<Item> itemsMappingConfig;
 
 	@GetMapping
-	public List<Item> findAll(@RequestParam(required = false) final String condition) {
-		//TODO: add pagination
-		return itemService.findAll();
-	}
-
-	@GetMapping("/{id}")
-	public Item findById(@PathVariable final Integer id) {
-		return itemService.findById(id);
+	public List<Item> findAll() {
+		return itemsMappingConfig;
 	}
 
 }
