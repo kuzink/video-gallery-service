17510d5a865c9162bbec162814041586fafb8179
kuzink <kuzink11@gmail.com>
2023-03-21 16:04:39 +0300

    Mod: updated spinner gifs

--- browser-app/src/components/utilities/spinner/Spinner.jsx
+++ browser-app/src/components/utilities/spinner/Spinner.jsx
@@ -1,18 +1,9 @@
 import React, { useEffect, useState } from "react";
-import icon_0 from '../../../assets/100x100/2.gif';
-import icon_1 from '../../../assets/100x100/2.gif';
+import icon_0 from '../../../assets/100x100/0.gif';
+import icon_1 from '../../../assets/100x100/1.gif';
 import icon_2 from '../../../assets/100x100/2.gif';
-import icon_3 from '../../../assets/100x100/2.gif';
-import icon_4 from '../../../assets/100x100/2.gif';
-import icon_5 from '../../../assets/100x100/23.gif';
-import icon_6 from '../../../assets/100x100/23.gif';
-import icon_7 from '../../../assets/100x100/23.gif';
-import icon_8 from '../../../assets/100x100/23.gif';
-import icon_9 from '../../../assets/100x100/23.gif';
 
-// Best loading gifs: 1 2 4 7 9 10 12 14 20 23 24 31 33 34 38 45 46 48 49 51 52 55 58 61 65 75 78 81 82
-
-const icons = [icon_0, icon_1, icon_2, icon_3, icon_4, icon_5, icon_6, icon_7, icon_8, icon_9];
+const icons = [icon_0, icon_1, icon_2];
 
 const Spinner = (props) => {
 
