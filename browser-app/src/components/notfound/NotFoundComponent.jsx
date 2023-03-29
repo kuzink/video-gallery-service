83cfc8eb24e4714c4045354b406852fcf76b9ef9
kuzink <kuzink11@gmail.com>
2023-03-29 17:54:59 +0300

    Mod: NotFoundComponent style change

--- browser-app/src/components/notfound/NotFoundComponent.jsx
+++ browser-app/src/components/notfound/NotFoundComponent.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import HomeButtonComponent from "../utilities/homebutton/HomeButtonComponent";
 
 const NotFoundComponent = () => (
-	<div className="custom-not-found-wrapper d-flex align-items-center pb-5">
-		<div className="container">
-			<div className="row">
-				<div className="col-4 mx-auto p-5 bg-white rounded shadow text-center">
-					<h1 className="font-weight-light">Not found</h1>
-					<p className="display-2 text-muted">404</p>
-					<h5 className="font-weight-normal mb-5">This page does not exist<br/>or is temporarily unavailable</h5>
-					<HomeButtonComponent/>
-				</div>
+	<div className="not-found-overlay">
+		<div className="not-found-wrapper">
+			<div className="not-found-header">
+				<h4>Video Gallery</h4>
+			</div>
+			<div className="not-found-body">
+				<h1>Not found</h1>
+				<p>404</p>
+				<h5>This page does not exist<br/>or is temporarily unavailable</h5>
+				<HomeButtonComponent/>
 			</div>
 		</div>
 	</div>
