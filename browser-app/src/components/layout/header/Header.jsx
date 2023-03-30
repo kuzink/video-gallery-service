5c8d62a64529b8e1e8c3af290f8ca001741e3fbc
kuzink <kuzink11@gmail.com>
2023-03-30 13:37:27 +0300

    Add: header draft

--- browser-app/src/components/layout/header/Header.jsx
+++ browser-app/src/components/layout/header/Header.jsx
@@ -6,7 +6,23 @@ export class Header extends Component {
     render() {
         return (
             <div className="test-header">
-	            <HomeButtonComponent classNames="ml-3"/>
+	            {/*<HomeButtonComponent classNames="ml-3"/>*/}
+
+	            <div className="header-logo-section">
+                    Logo here...
+                </div>
+
+	            <div className="header-toolbar-section">
+                    <div className="header-toolbar-section-inner">
+	                    <div className="header-left">
+		                    Sidebar collapse icon and search here...
+	                    </div>
+	                    <div className="header-right">
+		                    Profile and some action-buttons here...
+	                    </div>
+                    </div>
+                </div>
+
             </div>
         )
     }
