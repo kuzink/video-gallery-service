70981a1cb30f099ca5b194786b2148a9a2bc8cce
kuzink <kuzink11@gmail.com>
2023-04-02 22:37:25 +0300

    Add: header search draft

--- browser-app/src/components/layout/header/Header.jsx
+++ browser-app/src/components/layout/header/Header.jsx
@@ -2,10 +2,27 @@ import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import Logo from "../../../assets/logo6.png";
 import MenuIcon from "../../../assets/menu.svg";
+// import constants from "../../../constants/Constants";
 
 export class Header extends Component {
 
+	handleKeyPress = (event) => {
+		console.log("Header.handleKeyPress()");
+		if(event.key === 'Enter'){
+			// const search = searchText.trim().toLowerCase();
+			// return getItems(category, constants.PAGE_SIZE_DEFAULT_VALUE, constants.PAGE_NUMBER_DEFAULT_VALUE, sortBy, search);
+			console.log("Enter pressed");
+		}
+	};
+
+	handleSearchTextChange = (event) => {
+		// this.props.setSearchText(event.target.value)
+		console.log("Header.handleSearchTextChange()");
+	};
+
     render() {
+	    const searchText = "";
+
         return (
             <div className="test-header">
 	            <div className="header-inner">
@@ -24,7 +41,24 @@ export class Header extends Component {
 					                <img src={MenuIcon} alt=""/>
 				                </div>
 					            <div className="header-search">
-						            Search here...
+							            {/*<input className="form-control custom-search-items-input"*/}
+							                   {/*type="text"*/}
+							                   {/*placeholder="Search"*/}
+							                   {/*value={searchText}*/}
+							                   {/*onKeyPress={this.handleKeyPress}*/}
+							                   {/*onChange={this.handleSearchTextChange}/>*/}
+						            <form>
+							            <div className="input-group">
+								            <input type="search"
+								                   className="form-control dropdown-toggle"
+								                   placeholder="Search..."
+								                   id="top-search"/>
+								            <i className="fa-regular fa-magnifying-glass"/>
+								            <button className="input-group-text btn btn-primary" type="submit">
+									            Search
+								            </button>
+							            </div>
+						            </form>
 					            </div>
 					            <div className="clearfix"/>
 				            </div>
