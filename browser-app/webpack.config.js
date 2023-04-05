718f528c722d848373b0a6003bf4a854482907b7
kuzink <kuzink11@gmail.com>
2023-04-05 16:57:11 +0300

    Mod: changed avatar, webpack fix

--- browser-app/webpack.config.js
+++ browser-app/webpack.config.js
@@ -89,7 +89,7 @@ module.exports = {
             template: __dirname + '/src/index.html',
             filename: 'index.html',
             inject: 'body',
-            favicon: './src/assets/favicon.ico'
+            favicon: './src/assets/img/favicon.ico'
         })
     ]
 };
